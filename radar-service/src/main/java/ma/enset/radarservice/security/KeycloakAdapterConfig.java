27ddf7ad468b6e68f1b3968a98dea021ff8cd504
loubnaAminou <loubnaaminou0@gmail.com>
2022-12-25 23:58:25 +0100

    add security layer using Keycloak

---
+++ radar-service/src/main/java/ma/enset/radarservice/security/KeycloakAdapterConfig.java
@@ -0,0 +1,13 @@
+package ma.enset.radarservice.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakAdapterConfig {
+    @Bean
+    KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
